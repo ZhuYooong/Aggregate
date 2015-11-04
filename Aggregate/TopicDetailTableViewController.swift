ababe349ded99d516e9fd5ffddc1d1a4fa9c32ba
跳跃之狮 <mac@CMCC.local>
2015-11-04 17:36:02 +0800

    topicDetail

--- Aggregate/TopicDetailTableViewController.swift
+++ Aggregate/TopicDetailTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PKHUD
 
 class TopicDetailTableViewController: UITableViewController {
     @IBOutlet weak var headerTitleLable: UILabel!
@@ -17,8 +18,10 @@ class TopicDetailTableViewController: UITableViewController {
     @IBOutlet weak var headerMemberNameLable: UILabel!
     @IBOutlet weak var headerTitleHeight: NSLayoutConstraint!
     @IBOutlet weak var headerContentHeight: NSLayoutConstraint!
+    @IBOutlet weak var headerView: UIView!
     var topicId: String?
-    var pageNum = 0
+    var pageNum = 0//页码
+    var repliesArray = [Replies]()
     override func viewDidLoad() {
         super.viewDidLoad()
         initData()
@@ -26,74 +29,92 @@ class TopicDetailTableViewController: UITableViewController {
     //MARK:-初始化数据
     func initData() {
         if let topicId = topicId {
+            PKHUD.sharedHUD.contentView = PKHUDProgressView()
+            PKHUD.sharedHUD.show()
             TopicDetailViewModel.shareTopicDetailViewModel().findTopicsInfo(topicId) {
-                (contentArray) in
-                
+                (content) in
+                if let content = content {
+                    self.initTopicInfo(content)
+                }
             }
             TopicDetailViewModel.shareTopicDetailViewModel().findReplies(topicId, page: "\(pageNum)") {
                 (contentArray) in
-                
+                self.repliesArray = contentArray!
+                self.tableView.reloadData()
             }
         }
     }
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    func initTopicInfo(content: Topic) {//初始化话题内容
+        headerTitleLable.text = content.title
+        let option = NSStringDrawingOptions.UsesLineFragmentOrigin
+        let attributes = [NSFontAttributeName: UIFont.systemFontOfSize(17)]
+        headerTitleHeight.constant = headerTitleLable.text!.boundingRectWithSize(CGSizeMake(headerTitleLable.frame.size.width, 1000), options: option, attributes: attributes, context: nil).size.height + 2
+        if let renderedContent = content.content_rendered!.dataUsingEncoding(NSUTF32StringEncoding, allowLossyConversion: false) {
+            do {
+                headerContentLable.attributedText = try NSAttributedString(data: renderedContent, options: [NSDocumentTypeDocumentAttribute : NSHTMLTextDocumentType], documentAttributes: nil)
+            } catch let error1 as NSError {
+                print(error1)
+            }
+            headerContentHeight.constant = headerContentLable.attributedText!.boundingRectWithSize(CGSizeMake(headerContentLable.frame.size.width, 1000), options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil).size.height + 2
+        }
+        headerRepliesLable.text = content.replies
+        if let imageURL = content.member_avatar_mini {
+            ImageLoader.sharedLoader.imageForUrl("https:\(imageURL)", completionHandler:{(image: UIImage?, url: String) in
+                self.headerMemberImageView.image = image
+            })
+        }
+        headerMemberNameLable.text = content.member_username
+        headerView.bounds = CGRectMake(0, 0, UIScreen.mainScreen().bounds.size.width, 54 + headerContentHeight.constant + headerTitleHeight.constant)
+        tableView.tableFooterView = UIView()
+        tableView.tableHeaderView = headerView
+        view.layoutIfNeeded()
+        self.tableView.reloadData()
     }
-
-    /*
+    //MARK:-回复tableView代理
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repliesArray.count
+    }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("TopicDetailCell", forIndexPath: indexPath) as! TopicDetailTableViewCell
+        if repliesArray.count > indexPath.row {
+            if let imageURL = repliesArray[indexPath.row].member_avatar_mini {
+            ImageLoader.sharedLoader.imageForUrl("https:\(imageURL)", completionHandler:{(image: UIImage?, url: String) in
+                    cell.memberImageView.image = image
+                })
+            }
+            cell.memberNameLable.text = repliesArray[indexPath.row].member_username
+            let option = NSStringDrawingOptions.UsesLineFragmentOrigin
+            let attributes = [NSFontAttributeName: UIFont.systemFontOfSize(15)]
+            cell.memberNameWidth.constant = cell.memberNameLable.text!.boundingRectWithSize(CGSizeMake(1000, 18), options: option, attributes: attributes, context: nil).size.width + 2
+            if let timeStr = Double(repliesArray[indexPath.row].created!) {
+                let date = NSDate(timeIntervalSince1970: timeStr)
+                cell.timeLable.text = HomeViewModel.shareHomeViewModel().initDate(date)
+            }
+            if let contentData = repliesArray[indexPath.row].content_rendered!.dataUsingEncoding(NSUTF32StringEncoding, allowLossyConversion: false) {
+                do {
+                    cell.contentLable.attributedText = try NSAttributedString(data: contentData, options: [NSDocumentTypeDocumentAttribute : NSHTMLTextDocumentType], documentAttributes: nil)
+                } catch let error1 as NSError {
+                    print(error1)
+                }
+                cell.contentHeight.constant = cell.contentLable.attributedText!.boundingRectWithSize(CGSizeMake(UIScreen.mainScreen().bounds.size.width - 52, 1000), options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil).size.height + 2
+            }
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if repliesArray.count > indexPath.row {
+            if let contentData = repliesArray[indexPath.row].content_rendered!.dataUsingEncoding(NSUTF32StringEncoding, allowLossyConversion: false) {
+                do {
+                    return try NSAttributedString(data: contentData, options: [NSDocumentTypeDocumentAttribute : NSHTMLTextDocumentType], documentAttributes: nil).boundingRectWithSize(CGSizeMake(UIScreen.mainScreen().bounds.size.width - 52, 1000), options: NSStringDrawingOptions.UsesLineFragmentOrigin, context: nil).size.height + 44
+                } catch let error1 as NSError {
+                    print(error1)
+                    return 71
+                }
+            }else {
+                return 71
+            }
+        }else {
+            return 0
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Aggregate/TopicDetailViewModel.swift
+++ Aggregate/TopicDetailViewModel.swift
@@ -16,18 +16,17 @@ class TopicDetailViewModel: NSObject {
         return topicDetailViewModel
     }
     //MARK:-网络请求
-    func findTopicsInfo(topicId: String, initData: (contentArray: [Topic]?)->Void) {//取主题信息
+    func findTopicsInfo(topicId: String, initData: (contentArray: Topic?)->Void) {//取主题信息
         NetDataManager.shareNetDataManager().findTopicsInfo(topicId) {
             (data) in
-            var topicInfos = [Topic]()
             if let data = data {
                 let json = JSON(data: data)
-                for subJson in json.arrayValue {
+                if json.arrayValue.count > 0 {
+                    let subJson = json.arrayValue[0]
                     let topic = Topic(id: subJson["id"].stringValue, title: subJson["title"].stringValue, url: subJson["url"].stringValue, content: subJson["content"].stringValue, content_rendered: subJson["content_rendered"].stringValue, replies: subJson["replies"].stringValue, member_id: subJson["member"]["id"].stringValue, node_id: subJson["node"]["id"].stringValue, created: subJson["created"].stringValue, last_modified: subJson["last_modified"].stringValue, last_touched: subJson["last_touched"].stringValue, member_username: subJson["member"]["username"].stringValue, member_tagline: subJson["member"]["tagline"].stringValue, member_avatar_mini: subJson["member"]["avatar_mini"].stringValue, member_avatar_normal: subJson["member"]["avatar_normal"].stringValue, member_avatar_large: subJson["member"]["avatar_large"].stringValue, node_name: subJson["node"]["name"].stringValue, node_title: subJson["node"]["title"].stringValue, node_url: subJson["node"]["url"].stringValue, node_topics: subJson["node"]["topics"].stringValue, node_avatar_mini: subJson["node"]["avatar_mini"].stringValue, node_avatar_normal: subJson["node"]["avatar_normal"].stringValue, node_avatar_large: subJson["node"]["avatar_large"].stringValue)
-                    topicInfos.append(topic)
+                    initData(contentArray: topic)
                 }
             }
-            initData(contentArray: topicInfos)
         }
     }
     func findReplies(topicId: String, page: String?, initData: (contentArray: [Replies]?)->Void) {//取主题回复
